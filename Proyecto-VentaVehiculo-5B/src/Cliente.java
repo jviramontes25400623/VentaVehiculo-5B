19a787971840e94be5931749b641d729d86bc29c
Viramontes Maldonado Juan Antonio <juanviramontesma.ittepic.edu.mx>
2026-02-13 12:51:30 -0700

    feat :  implementa clase Vehiculo

--- Proyecto-VentaVehiculo-5B/src/Cliente.java
+++ Proyecto-VentaVehiculo-5B/src/Cliente.java
@@ -8,27 +8,23 @@
  * @author juani
  */
 public class Cliente {
-   
-         
+
     //rs=razon social
-    
     private String rs = "";
     private String rfc = "";
     private String email = "";
-    
-    public Cliente(String rs, String rfc, String email){
-    this.rs = rs;
-    this.rfc = rfc;
-    this.email = email;
-    
-    
+
+    public Cliente(String rs, String rfc, String email) {
+        this.rs = rs;
+        this.rfc = rfc;
+        this.email = email;
+
     }
+
     @Override
-    public String toString(){
-    return this.rs + this.rfc + this.email;
+    public String toString() {
+        return this.rs + this.rfc + this.email;
     }
-    
 
     //atributos, constructor, toString
-    
 }
